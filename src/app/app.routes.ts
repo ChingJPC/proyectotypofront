68fec5eb7d992ee17b7035c494e588bae842493c
ChingJPC <programaconching@gmail.com>
2024-01-23 21:53:39 -0500

    Primer comit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { BodyComponent } from './inicio/body/body.component';
+import { IndexComponent as IndexUsuario } from './usuarios/index/index.component';
+import { IndexComponent as IndexRol } from './roles/index/index.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'inicio/body', pathMatch: 'full' },
+    { path: 'inicio/body', component: BodyComponent },
+
+    { path: 'usuario/index', component: IndexUsuario},
+    
+    { path: 'rol/index', component: IndexRol},
+    { path: 'rol/index', component: IndexRol},
+    { path: 'rol/index', component: IndexRol},
+];
